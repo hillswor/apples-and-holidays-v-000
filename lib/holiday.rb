25bc7b05cb226034b044e81b23485afc36739fb0
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-04 21:25:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       if supplies == "BBQ"
-        holiday
+        puts holiday
       end
     end
   end
