7f67222db08789a0e896959c991be208d3a91fdb
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-04 21:31:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,21 +1,5 @@
 require 'pry'
 
-holiday_supplies = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
-
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
@@ -68,4 +52,3 @@ def all_holidays_with_bbq(holiday_hash)
     end
   end
 end
-binding.pry
