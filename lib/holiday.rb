a8c10a5450e9ee2d778b7cefce00fe907ee5f103
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-04 21:12:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supply_list|
-      holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join("") << ":"
+      holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ") << ":"
       supplies = supply_list.join("")
       puts "#{holiday} #{supplies}"
     end
